724c72a5b24d115d5298186c05f3a2b91f64e185
Ted Gould <ted@gould.cx>
2012-08-17 11:39:11 -0500

    Understanding the design

--- src/pam-freerdp.c
+++ src/pam-freerdp.c
@@ -1,18 +1,25 @@
 #include <security/pam_modules.h>
 #include <security/pam_modutil.h>
 
+/* Authenticate.  We need to make sure we have a user account, that
+   there are remote accounts and then verify them with FreeRDP */
 PAM_EXTERN int
 pam_sm_authenticate (pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
 	return PAM_IGNORE;
 }
 
+/* Open Session.  Here we need to fork a little process so that we can
+   give the credentials to the session itself so that it can startup the
+   xfreerdp viewer for the login */
 PAM_EXTERN int
 pam_sm_open_session (pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
      return PAM_IGNORE;
 }
 
+/* Close Session.  Make sure our little guy has died so he doesn't become
+   a zombie and eat things. */
 PAM_EXTERN int
 pam_sm_close_session (pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
