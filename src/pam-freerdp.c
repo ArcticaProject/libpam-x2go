fc6dac468200850eda982bc3a9f4d7f4a0cf4311
Ted Gould <ted@gould.cx>
2012-08-30 11:55:41 -0500

    Make sure to clear the environments

--- src/pam-freerdp.c
+++ src/pam-freerdp.c
@@ -238,6 +238,10 @@ pam_sm_authenticate (pam_handle_t *pamh, int flags, int argc, const char **argv)
 			_exit(EXIT_FAILURE);
 		}
 
+		if (clearenv() != 0) {
+			_exit(EXIT_FAILURE);
+		}
+
 		setenv("HOME", pwdent->pw_dir, 1);
 
 		execvp(args[0], args);
@@ -297,6 +301,11 @@ session_socket_handler (struct passwd * pwdent, int readypipe, const char * ruse
 
 	if (setgid(pwdent->pw_gid) < 0 || setuid(pwdent->pw_uid) < 0 ||
 			setegid(pwdent->pw_gid) < 0 || seteuid(pwdent->pw_uid) < 0) {
+		/* Don't need to clean up yet */
+		return EXIT_FAILURE;
+	}
+
+	if (clearenv() != 0) {
 		/* Don't need to clean up yet */
 		return EXIT_FAILURE;
 	}
